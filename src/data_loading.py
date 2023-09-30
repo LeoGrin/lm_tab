645b852fa7fd9eecc07ef6c5474a0ffb7dd81919
LeoGrin <leo.grinsztajn@polytechnique.edu>
2023-09-30 19:26:31 +0200

    remove missing data

--- src/data_loading.py
+++ src/data_loading.py
@@ -3,6 +3,18 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+def remove_missing_values(X, y, threshold=0.7):
+    """Remove columns where most values are missing, then remove any row with missing values"""
+    missing_cols_mask = pd.isnull(X).mean(axis=0) > threshold
+    print("Removed {} columns with missing values on {} columns".format(sum(missing_cols_mask), X.shape[1]))
+    X = X.drop(X.columns[missing_cols_mask], axis=1)
+    missing_rows_mask = pd.isnull(X).any(axis=1)
+    print("Removed {} rows with missing values on {} rows".format(sum(missing_rows_mask), X.shape[0]))
+    X = X[~missing_rows_mask]
+    y = y[~missing_rows_mask]
+    res = (X, y, missing_cols_mask, missing_rows_mask)
+
+
 default_column = {"employee_salary": "employee_position_title",
                     "drug_directory": "SUBSTANCENAME",
                     #"readability": "excerpt",
@@ -12,7 +24,7 @@ skrub_functions = {"employee_salary": fetch_employee_salaries,
                      "drug_directory": fetch_drug_directory
 }
 
-def load_data(data_name, max_rows=None, include_all_columns=False):
+def load_data(data_name, max_rows=None, include_all_columns=False, remove_missing=True):
     # load the data
     if data_name in skrub_functions.keys():
         ds = skrub_functions[data_name]()
@@ -53,6 +65,12 @@ def load_data(data_name, max_rows=None, include_all_columns=False):
     
     X_text = X[default_col]
     X_rest = X.drop(default_col, axis=1)
+    if remove_missing:
+        X_rest, y, missing_cols_mask, missing_rows_mask = remove_missing_values(X_rest, y)
+        X_text = X_text[~missing_rows_mask]
+        print("Removed {} rows with missing values on {} rows".format(sum(missing_rows_mask), X.shape[0]))
+        print("Removed {} columns with missing values on {} columns".format(sum(missing_cols_mask), X.shape[1] - 1))
+        print("New shape: {}".format(X_rest.shape))
     if include_all_columns:
         return X_text, X_rest, y
     else:
